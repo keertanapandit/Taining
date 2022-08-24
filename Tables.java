5437acf02d351944bfe175de21d4413bc07fc295
keertanapandit <75423482+keertanapandit@users.noreply.github.com>
2022-08-24 10:03:56 +0530

    Update and rename Table.java to Tables.java

--- Tables.java
+++ Tables.java
@@ -3,10 +3,14 @@ class Main{
     public static void cal(int n){
         int i;
         int prod;
+        int sum=0;
         for(i=1;i<=10;i++){
            prod=n*i;
-        System.out.println(prod+"\n");
-    }
+           sum+=prod;
+            System.out.println(prod+"\n");
+        }
+       
+        System.out.println(sum+"\n");
 }
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
